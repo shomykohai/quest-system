57f1ecb7a560fdd67af5b258a18baa86a328c7a7
Shomy <61943525+ShomyKohai@users.noreply.github.com>
2024-01-12 02:28:24 +0100

    Use the correct script for autoload
    Oops, messed up something

--- addons/quest_system/plugin.gd
+++ addons/quest_system/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_autoload_singleton("QuestSystem", "plugin.gd")
+	add_autoload_singleton("QuestSystem", "quest_manager.gd")
 
 
 func _exit_tree() -> void:
