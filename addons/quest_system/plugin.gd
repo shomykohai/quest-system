3186567616ee5ab21ff77b64422518d31e5f9f9f
Shomy <61943525+shomykohai@users.noreply.github.com>
2024-05-06 00:27:13 +0200

    Add auto updater (#15)

--- addons/quest_system/plugin.gd
+++ addons/quest_system/plugin.gd
@@ -1,8 +1,10 @@
 @tool
 extends EditorPlugin
 
+const REMOTE_RELEASE_URL: String = "https://api.github.com/repos/shomykohai/quest-system/releases/latest"
 const QuestPropertyTranslationPlugin = preload("./translation_plugin.gd")
 
+var update_button: Button = null
 var translation_plugin: QuestPropertyTranslationPlugin
 
 func _enter_tree() -> void:
@@ -10,9 +12,65 @@ func _enter_tree() -> void:
 	if Engine.is_editor_hint():
 		translation_plugin = QuestPropertyTranslationPlugin.new()
 		add_translation_parser_plugin(translation_plugin)
-
+		# Check for new version
+		var http_request := HTTPRequest.new()
+		http_request.request_completed.connect(_http_request_completed)
+		EditorInterface.get_base_control().add_child(http_request)
+		http_request.request(REMOTE_RELEASE_URL)
+		await http_request.request_completed
+		http_request.queue_free()
 
 func _exit_tree() -> void:
 	remove_autoload_singleton("QuestSystem")
 	remove_translation_parser_plugin(translation_plugin)
 	translation_plugin = null
+
+func _version_to_int(ver: String) -> int:
+	var parts: Array = ver.split(".")
+	return parts[0].to_int() * 100 + parts[1].to_int() * 10 + parts[2].to_int()
+
+func _get_plugin_icon() -> Texture2D:
+	return load(_get_plugin_path() + "/assets/quest_resource.svg")
+
+func _get_plugin_path() -> String:
+	return get_script().resource_path.get_base_dir()
+
+func _http_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray):
+	if result != HTTPRequest.RESULT_SUCCESS:
+		return
+	
+	var data: Dictionary = JSON.parse_string(body.get_string_from_utf8())
+	if _version_to_int(data["tag_name"]) <= _version_to_int(get_plugin_version()):
+		return
+	
+	var title_bar: Node = null
+	
+	# Here we get the title bar node in a unconventional way
+	for node in EditorInterface.get_base_control().get_children(true):
+		if node is VBoxContainer:
+			title_bar = node.get_children(true)[0]
+			break
+
+	update_button = Button.new()
+	update_button.add_theme_color_override("font_color", Color.LIME_GREEN)
+	update_button.text = data["tag_name"]
+	update_button.icon = _get_plugin_icon()
+	update_button.pressed.connect(_on_update_button_pressed.bind(get_plugin_version(), data["tag_name"]))
+	title_bar.add_child(update_button)
+	title_bar.move_child(update_button, title_bar.get_child_count(true) - 3)
+	
+func _on_update_button_pressed(old_ver: String, new_ver: String) -> void:
+	var update_panel: AcceptDialog = load(_get_plugin_path()+"/views/update_dialog.tscn").instantiate()
+	update_panel.set_meta("old_ver", old_ver)
+	update_panel.set_meta("new_ver", new_ver)
+	update_panel.set_meta("plugin_path", _get_plugin_path())
+	update_button.add_child(update_panel)
+	update_panel.popup_centered()
+	update_panel.prepare()
+	await update_panel.updated
+	update_panel.queue_free()
+	update_button.queue_free()
+	EditorInterface.get_resource_filesystem().scan()
+	EditorInterface.call_deferred("set_plugin_enabled", "quest_system", true)
+	EditorInterface.set_plugin_enabled("quest_system", false)
+	print_rich("[color=cyan][!][/color] [b]QuestSystem updated to version: [color=cyan]%s[/color][/b]" % new_ver)
---
+++ addons/quest_system/views/update_dialog.gd
@@ -0,0 +1,74 @@
+@tool
+extends AcceptDialog
+
+signal updated
+
+const DOWNLOAD_URL: String = "https://github.com/shomykohai/quest-system/archive/refs/tags/%s.zip"
+const TEMP_FILE_PATH: String = "user://quest_system_temp.zip"
+
+@onready var version_label: RichTextLabel = %VersionLabel
+@onready var download_button: Button = %DownloadButton
+@onready var http_request: HTTPRequest = %HTTPRequest
+
+var new_ver: String = ""
+var old_ver: String = ""
+var plugin_path: String = ""
+
+
+func prepare() -> void:
+	new_ver = get_meta("new_ver")
+	old_ver = get_meta("old_ver")
+	plugin_path = get_meta("plugin_path")
+	version_label.text = version_label.text % [new_ver, old_ver]
+	%LinkButton.uri = "https://github.com/shomykohai/quest-system/releases/tag/%s" % new_ver
+	title = new_ver
+
+
+func _save_zip(bytes: PackedByteArray) -> void:
+	var file := FileAccess.open(TEMP_FILE_PATH, FileAccess.WRITE)
+	file.store_buffer(bytes)
+	file.close()
+
+
+func _on_download_button_pressed():
+	http_request.request(DOWNLOAD_URL % new_ver)
+	download_button.text = "Downloading..."
+	download_button.disabled = true
+
+
+func _on_http_request_request_completed(result, response_code, headers, body):
+	if result != HTTPRequest.RESULT_SUCCESS:
+		download_button.text = "Download failed. Try again later."
+		download_button.disabled = false
+		return
+
+	_save_zip(body)
+
+	var zip := ZIPReader.new()
+	zip.open(TEMP_FILE_PATH)
+	var files := zip.get_files()
+
+	var zip_base_path: String = files[1]
+	var save_path: String = plugin_path.get_base_dir()
+
+	# Remove archive and repo base path from file list
+	files.remove_at(0)
+	files.remove_at(0)
+
+	OS.move_to_trash(ProjectSettings.globalize_path(plugin_path))
+
+	for path in files:
+		var new_path := path.replace(zip_base_path, "")
+		if path.ends_with("/"):
+			DirAccess.make_dir_recursive_absolute(save_path + "/%s" % new_path)
+		else:
+			var file := FileAccess.open(save_path + "/%s" % new_path, FileAccess.WRITE)
+			file.store_buffer(zip.read_file(path))
+
+	zip.close()
+	DirAccess.remove_absolute(TEMP_FILE_PATH)
+
+	updated.emit()
+
+	download_button.text = "Updated!"
+	version_label.text = "You're running the last version of QuestSystem."
