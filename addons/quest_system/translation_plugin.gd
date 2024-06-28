662ce2ea83db5882bfb94ecea8bdbc53c12546ff
Shomy <61943525+shomykohai@users.noreply.github.com>
2024-06-28 18:53:38 +0200

    Fix TranslationPlugin property usage comparison for exported variables

--- addons/quest_system/translation_plugin.gd
+++ addons/quest_system/translation_plugin.gd
@@ -7,7 +7,7 @@ func _parse_file(path: String, msgids: Array[String], msgids_context_plural: Arr
 	for property in res.get_script().get_script_property_list():
 		if property.type != 4: continue # If the property is not a string, we skip it
 		# Here we check if the property is an exported variable
-		if property.usage & PROPERTY_USAGE_STORAGE or property.usage & PROPERTY_USAGE_SCRIPT_VARIABLE:
+		if property.usage == PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_STORAGE | PROPERTY_USAGE_EDITOR:
 			msgids.append("quest_%s/%s"% [res.id, property["name"]]) # quest_1/quest_name
 
 func _get_recognized_extensions() -> PackedStringArray:
