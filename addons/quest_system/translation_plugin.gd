5b41599bafaec9b34385296ce91d287c95d51cd9
Shomy 🍂 <61943525+shomykohai@users.noreply.github.com>
2025-03-09 23:12:47 +0100

    Fix TranslationPlugin functions for Godot 4.4+

--- addons/quest_system/translation_plugin.gd
+++ addons/quest_system/translation_plugin.gd
@@ -1,14 +1,24 @@
 extends EditorTranslationParserPlugin
 
-# func _parse_file(path: String, msgids: Array[String], msgids_context_plural: Array[Array]) -> void:
-# 	var res := ResourceLoader.load(path)
-# 	if not res: return
-# 	if not res is Quest: return
-# 	for property in res.get_script().get_script_property_list():
-# 		if property.type != 4: continue # If the property is not a string, we skip it
-# 		# Here we check if the property is an exported variable
-# 		if property.usage == PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_STORAGE | PROPERTY_USAGE_EDITOR:
-# 			msgids.append("quest_%s/%s"% [res.id, property["name"]]) # quest_1/quest_name
+func _parse_file(path: String) -> Array[PackedStringArray]:
+	var res := ResourceLoader.load(path)
+	if not res: return []
+	if not res is Quest: return []
+	
+	var ret: Array[PackedStringArray] = []
+	for property in res.get_script().get_script_property_list():
+		var msgids: PackedStringArray = []
+		if property.type != 4: continue # If the property is not a string, we skip it
+
+		# Here we check if the property is an exported variable
+		if property.usage == PROPERTY_USAGE_SCRIPT_VARIABLE | PROPERTY_USAGE_STORAGE | PROPERTY_USAGE_EDITOR:
+			msgids.append("quest_%s/%s"% [res.id, property.name]) # quest_1/quest_name
+			msgids.append("Quest ID: %s, property: %s"% [res.id, property.name]) # quest_1/quest_objective
+			msgids.append("quest_%s/%s_plural"% [res.id, property.name]) # quest_1/quest_description
+
+			ret.append(msgids)
+
+	return ret
 
 func _get_recognized_extensions() -> PackedStringArray:
 	return ['tres']
